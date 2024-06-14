8f09e2838a361adaba5ca08fd0f4ecb1a9d9d014
Dalmas <akumuodhiambo@gmail.com>
2024-06-14 18:47:13 +0300

    Url updated

--- myplpecom/urls.py
+++ myplpecom/urls.py
@@ -5,7 +5,7 @@ app_name = 'myplpecom'
 
 urlpatterns = [
     path('product_list/', views.product_list, name='product_list'),
-    path('products/<int:id>/', views.product_detail, name='product_detail'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
     path('customer/', views.customer_list, name='customer_list'),
     path('customers/<int:pk>/', views.customer_detail, name='customer_detail'),
 ]
