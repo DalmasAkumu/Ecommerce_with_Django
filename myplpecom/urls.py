c86ca9101b5eeaeff0bb82d927b13cfab4d1b78b
Dalmas <akumuodhiambo@gmail.com>
2024-06-14 11:22:15 +0300

    Customer and Product detail elements added

--- myplpecom/urls.py
+++ myplpecom/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = 'myplpecom'
+
 urlpatterns = [
-  path('product_list', views.product_list, name= 'product_list'),
+    path('product_list/', views.product_list, name='product_list'),
+    path('products/<int:id>/', views.product_detail, name='product_detail'),
+    path('customer/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>/', views.customer_detail, name='customer_detail'),
+]
+
+urlpatterns = [
+    path('product_list/', views.product_list, name='product_list'),
+    path('products/<int:id>/', views.product_detail, name='product_detail'),
+    path('customer/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>/', views.customer_detail, name='customer_detail'),
 ]
--- myplpecom/views.py
+++ myplpecom/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import get_object_or_404, render
+from .models import Customer, Product
 #from django.shortcuts import HttpResponse
 
 # Create your views here.
@@ -13,3 +13,23 @@ def product_list(request):
   }
   return render(request, 'myplpecom/product_list.html', context)
 
+def product_detail(request, pk):
+  product = get_object_or_404(Product, pk=pk)
+  context = {
+    'product': product
+  }
+  return render (request, 'myplpecom/product_detail.html', context)
+
+def customer_list(request):
+  customers = Customer.objects.all()
+  context = {
+    'customers': customers
+  }
+  return render(request, 'myplpecom/customer_list.html', context)
+
+def customer_detail(request, pk):
+  customer = get_object_or_404(Customer, pk=pk)
+  context = {
+    'customer': customer
+  }
+  return render (request, 'myplpecom/customer_detail.html', context)
